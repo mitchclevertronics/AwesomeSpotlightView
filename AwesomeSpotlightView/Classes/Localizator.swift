8ef8b6c654c1238e0648167597c818c4ef20a887
Mark Turner <mt@papercloud.com.au>
2017-12-13 10:09:24 +1100

    Updating Localizator bundle path to reference the framework bundle - when installing using pods. #10

--- AwesomeSpotlightView/Classes/Localizator.swift
+++ AwesomeSpotlightView/Classes/Localizator.swift
@@ -13,8 +13,7 @@ private class Localizator {
   static let sharedInstance = Localizator()
   
   lazy var localizableDictionary: NSDictionary! = {
-    
-    if let bundlePath = Bundle.main.path(forResource: "AwesomeSpotlightViewBundle", ofType: "bundle") {
+    if let bundlePath = Bundle(for: AwesomeSpotlightView.self).path(forResource: "AwesomeSpotlightViewBundle", ofType: "bundle") {
       if let path = Bundle(path: bundlePath)?.path(forResource: "Localizable", ofType: "plist") {
         return NSDictionary(contentsOfFile: path)
       }
